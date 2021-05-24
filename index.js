161bb13efd82672263db1d61038f80a6863b7748
Sal5468 <145004342@vistausd.org>
2021-05-24 11:19:21 -0700

    Todays Work

--- index.js
+++ index.js
@@ -5,15 +5,47 @@ var app = express();
 var bodyParser = require('body-parser');
 var routes = require("./routes");
 var compAI = require('./AI');
+var cookieParser = require("cookie-parser");
+var flash = require("connect-flash");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var session = require("express-session");
+
+var setUpPassport = require("./setuppassport");
+var routes = require("./routes");
+
+var app = express();
+
+//27017 seems to be the port number used by mongod
+mongoose.connect("mongodb://localhost:27017/userdb");
+setUpPassport();
+
+app.use('/', express.static('./'));
+app.use('/js', express.static('./public/js'));
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+  secret: "LUp$Dg?,I#i&owP3=9su+OB%`JgL4muLF5YJ~{;t",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 //app.use('/', express.static('./'));
 app.use(express.static("./public"));
 
 app.use(routes);
-app.use(logger("short"));
+//app.use(logger("short"));
 
 //req is info sending to server from client.
 //res is info sending to client from server.
--- routes.js
+++ routes.js
@@ -5,7 +5,10 @@ var formidable = require('formidable');
 var mv = require('mv');
 var AI = require('./AI');
 var boardInfo = require('./BoardInfo');
+var passport = require("passport");
+var path = require("path");
 
+var User = require("./models/user");
 
 let playercharchosen = ""//delete this
 let aiplayerchosen = ""//delete this
@@ -14,8 +17,13 @@ let Client = [];
 let newClientId = 0;
 
 router.get("/",function(req,res){
+	res.sendFile(__dirname + "/public/views/login.html");
+});
+router.get("/signup",function(req,res){
+	res.sendFile(__dirname + "/public/views/signup.html");
+});
+router.get("/menu",function(req,res){
 	res.sendFile(__dirname + "/public/views/index.html");
-	//Clint[] = new Ai.js
 });
 router.get("/playvsai",function(req,res){
   res.sendFile(__dirname + "/public/views/vsai.html");
@@ -101,5 +109,131 @@ router.get("/makeaguess",function(req,res)
 	}
 	res.json({winlose : result});
 });*/
+router.use(function(req, res, next) {
+  res.locals.currentUserjy = req.user;
+  res.locals.errors = req.flash("error");
+  res.locals.infos = req.flash("info");
+  next();
+});
+
+
+
+
+
+
+router.get("/successroot", function(req, res) {
+console.log("get successroot");
+	res.json({redirect:"/"});
+});
+
+router.get("/failroot", function(req, res) {
+console.log("get failroot");
+	res.json({redirect:"/login"});
+});
+
+router.get("/successsignup", function(req, res) {
+console.log("get successsignup");
+	res.json({redirect:"/menu"});
+});
+
+router.get("/failsignup", function(req, res) {
+console.log("get failsignup");
+	res.json({redirect:"/login"});
+});
+
+router.get("/successlogin", function(req, res) {
+console.log("get successlogin");
+	res.json({redirect:"/menu"});
+});
+router.get("/faillogin", function(req, res) {
+console.log("get failsignup");
+	res.json({redirect:"/login"});
+
+});
+
+router.get("/menu", function(req, res) {
+  console.log("get menu");
+  if (req.isAuthenticated()) {
+    console.log("sendFile menu.html")
+	let thePath = path.resolve(__dirname,"public/views/menu.html");
+	res.sendFile(thePath);
+  } else {
+    console.log("sendFile login.html")
+  	let thePath = path.resolve(__dirname,"public/views/login.html");
+	res.sendFile(thePath);
+  }
+});
+
+router.get("/userInfo",function(req,res){
+  console.log("get userInfo");
+     if (req.isAuthenticated()) {
+  console.log("req isAuthenticated");
+  console.log("valueJY = " + req.user.valueJY);    /* user defined value */
+		res.json({username:req.user.username});
+	}
+	else {
+  console.log("req is not Authenticated");
+		res.json(null);
+	}
+});
+
+
+
+
+router.get("/logout", function(req, res) {
+  console.log("get logout")
+  if (req.isAuthenticated()) {
+  console.log("req isAuthenticated");
+    req.logout();
+    res.redirect("/successroot");
+  } else {
+  console.log("req is not Authenticated");
+    res.redirect("/failroot");
+  }
+});
+
+router.post("/signup", function(req, res, next) {
+console.log("post signup");
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username }, function(err, user) {
+console.log("User findOne function callback")
+    if (err)
+    {
+      console.log("err");
+      return next(err);
+    }
+    if (user) {
+      console.log("user")
+      req.flash("error", "User already exists");
+      return res.redirect("/failsignup");
+    }
+console.log("new User")
+    var newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);    //goes to user.js (userSchema.pre(save))
+  });
+
+
+}, passport.authenticate("login", {       //goes to setuppassport.js  (passport.use("login"))
+  successRedirect: "/successsignup",
+  failureRedirect: "/failsignup",
+  failureFlash: true
+}));
+
+
+
+
+router.post("/login", passport.authenticate("login", {
+  successRedirect: "/successlogin",
+  failureRedirect: "/faillogin",
+  failureFlash: true
+}));
+
+
 
 module.exports = router;
