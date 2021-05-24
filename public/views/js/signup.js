71470e9cc58367a934a9c41f039f179fae152728
Sal5468 <145004342@vistausd.org>
2021-05-24 10:43:37 -0700

    some new files for login stuff

---
+++ public/views/js/signup.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+ 		
+  		function userClicked(){
+console.log("signup userClicked")
+
+
+          $.post("/signup",{username:$("#username").val(), password:$("#psw").val()},function(data)
+{
+  console.log("signup callback function")
+  window.location = data.redirect;
+});
+          
+    			return false;
+    		}
+
+
+  		$(document).ready(function(){ 
+
+        $("#username").keydown( function( event ) {
+            if ( event.which === 13 ) {
+              userClicked();
+              event.preventDefault();
+              return false;
+            }
+        });
+        
+        $("#psw").keydown( function( event ) {
+            if ( event.which === 13 ) {
+              userClicked();
+              event.preventDefault();
+              return false;
+            }
+        });
+
+  		});  		
+    
+
